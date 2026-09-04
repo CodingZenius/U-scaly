f513ea77453e20483a85f93758a02ffde14f61b3
CodingZenius <tekbizz@zohomail.com>
2026-09-04 13:44:12 +0100

    Create tsconfig.ts

---
+++ tsconfig.ts
@@ -0,0 +1,32 @@
+{
+  "compilerOptions": {
+    "target": "ES2022",
+    "module": "ESNext",
+    "moduleResolution": "Bundler",
+
+    "strict": true,
+    "noEmit": true,
+
+    "lib": [
+      "ES2022",
+      "DOM"
+    ],
+
+    "allowJs": true,
+    "checkJs": false,
+
+    "isolatedModules": true,
+    "skipLibCheck": true,
+
+    "forceConsistentCasingInFileNames": true
+  },
+
+  "include": [
+    "src/animation.ts"
+  ],
+
+  "exclude": [
+    "node_modules",
+    "dist"
+  ]
+}
