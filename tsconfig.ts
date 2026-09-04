8dbb632a8b6c1dc82809df5e35b6b96bb7a8ccc4
CodingZenius <tekbizz@zohomail.com>
2026-09-04 13:50:48 +0100

    Update tsconfig.ts

--- tsconfig.ts
+++ tsconfig.ts
@@ -1,32 +1,17 @@
 {
+  "extends": "expo/tsconfig.base",
   "compilerOptions": {
-    "target": "ES2022",
-    "module": "ESNext",
-    "moduleResolution": "Bundler",
-
     "strict": true,
     "noEmit": true,
-
-    "lib": [
-      "ES2022",
-      "DOM"
-    ],
-
     "allowJs": true,
     "checkJs": false,
-
-    "isolatedModules": true,
     "skipLibCheck": true,
-
     "forceConsistentCasingInFileNames": true
   },
-
   "include": [
-    "src/animation.ts"
-  ],
-
-  "exclude": [
-    "node_modules",
-    "dist"
+    "**/*.ts",
+    "**/*.tsx",
+    "**/*.js",
+    "**/*.jsx"
   ]
 }
